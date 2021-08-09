ec95c9ba1b8127d5f8d2fa2af5f9dbea9c6e2211
otis wilcox <otiswilcox@gmail.com>
2021-08-09 16:38:17 -0400

    added the function to add games to backlog

--- routes/backlog.js
+++ routes/backlog.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
 const authorize = require("../middleware/authorize");
 const pool = require("../db");
+const validInfo = require("../middleware/validInfo");
 
 // get backlog
 
 router.get("/", authorize, async (req, res) => {
   try {
     const backlog = await pool.query(
-      "SELECT user_name, game_id, date_added FROM backlog INNER JOIN users ON backlog.user_id = users.user_id WHERE users.user_id = $1",
+      "SELECT users.user_id, user_name, game_id, date_added, game_name, game_image FROM backlog INNER JOIN users ON backlog.user_id = users.user_id WHERE users.user_id = $1",
       [req.user.id]
     );
 
@@ -19,25 +20,28 @@ router.get("/", authorize, async (req, res) => {
   }
 });
 
-// router.post("/addGame", async (req, res) => {
-//   try {
-//     const { name, email, password } = req.body;
-//     const user = await pool.query("SELECT * FROM backlog WHERE user_id = users(user_id)", [
-//       email,
-//     ]);
+router.post("/", validInfo, async (req, res) => {
+  try {
+    const { game_id, user_id, date, game_name, game_image } = req.body;
+    console.log(req.body);
+    const game = await pool.query(
+      "SELECT * FROM backlog WHERE game_id = $1 AND user_id = $2",
+      [game_id, user_id]
+    );
 
-//     if (user.rows.length !== 0) {
-//       return res.status(401).json("User already exists");
-//     }
+    if (game.rows.length !== 0) {
+      return res.status(401).json("This game is already in your backlog!");
+    }
 
-//     const newGame = await pool.query(
-//       "INSERT INTO backlog (game_id, date_added, user_id) VALUES (360, '2021-08-08', 'e5daae2d-cfa2-49ac-a9f6-203ec4a9d783');",
-//       [name, email, bcryptPassword]
-//     );
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send("Server error");
-//   }
-// });
+    const newGame = await pool.query(
+      "INSERT INTO backlog (game_id, date_added, user_id, game_name, game_image) VALUES ($1, $2, $3, $4, $5);",
+      [game_id, date, user_id, game_name, game_image]
+    );
+    console.log("the post is:", newGame);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
